82e3fbd2937a01787dc8fa32989e02781045a198
Welile4Up <166120395+Welile4Up@users.noreply.github.com>
2024-04-05 14:35:04 +0200

    Add files via upload

---
+++ kidsbook/chapter2/chapter2.js
@@ -0,0 +1,219 @@
+// console.log(99*123);
+// console.log("This is a slice".slice(0,4));
+// console.log(true && false)
+
+//NUMBERS AND OPERATORS
+
+console.log("Adding " + (12345 + 56789));
+
+console.log("Adding three numbers " + (22 + 33 + 44));
+
+console.log("Subtracting " + (1000 - 17));
+
+console.log("Multiplication " + (123 * 456));
+
+console.log("Divison " + (12345 / 250));
+
+console.log(1234 + 57 * 3 - 31 / 4);
+
+console.log(8 / (1 + 3))
+
+//Try it out
+
+console.log((15 + 9) * 2);
+
+//VARIABLES
+
+// let age;
+// age = 12;
+
+// let age = 12;
+// age = 13
+// console.log("age is " + age);
+
+// let numberOfSiblings = 1 + 3;
+// let numberOfCandles = 8;
+// console.log(numberOfCandles / numberOfSiblings); 
+
+// let secondsInAMinute = 60;
+// let minutesInAnHour = 60;
+// let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+// console.log(secondsInAnHour);
+
+// let hoursInADay = 24;
+// let secondsInADay = secondsInAnHour * hoursInADay;
+// console.log(secondsInADay);
+
+// let daysInAYear = 365;
+// let secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
+
+// let age = 53;
+// let secondsInYourAge = age * secondsInAYear;
+// console.log(`You are ${secondsInYourAge} seconds old!`);
+// console.log("You are " + secondsInYourAge + " seconds old!");
+
+// let highFives = 0;
+// ++highFives;
+// console.log(highFives);
+// ++highFives;
+// console.log(highFives);
+// --highFives;
+// console.log(highFives);
+
+// let highFives = 0;
+// console.log(highFives++);
+// console.log(highFives--);
+// console.log(highFives);
+
+// let x = 1;
+// let y = 2;
+
+// // console.log(x++ + --y);
+// console.log(++x + y--);
+// console.log(x);
+// console.log(y); 
+
+//plus equal and minus equal to signs
+
+// let x = 10;
+// x = x + 5;
+// console.log(x);
+
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+// let score = 10;
+// score += 7;
+// console.log(score);
+// score -=3;
+// console.log(score);
+
+
+// let balloons = 100;
+// balloons *= 2;
+// console.log(balloons);
+
+// let balloons = 100;
+// balloons /= 4;
+// console.log(balloons);
+
+//Strings
+
+// console.log("Hello World!");
+
+// let myAwesomeString = "Something REALLY awesome!!!"
+// console.log(myAwesomeString);
+
+// let myThing = 5;
+// console.log(myThing);
+// myThing = "this is a string";
+// console.log(myThing);
+
+// let numberNine = 9;
+// let stringNine = "9";
+// console.log(numberNine + numberNine);
+// console.log(stringNine + stringNine);
+// console.log(numberNine + stringNine);
+
+// let greeting = "Hello ";
+// let myName = "Welile";
+// // console.log(greeting + myName);
+// console.log(greeting, myName);
+
+// console.log("Supercalifragilisticexpialidocious".length);
+
+// let myName = "Nick";
+// console.log(myName[0]);
+// console.log(myName[1]);
+// console.log(myName[2]);
+// console.log(myName[3]);
+// // console.log(myName[4]);
+
+// let codeWord1 = "are";
+// let codeWord2 = "tubas";
+// let codeWord3 = "unsafe";
+// let codeWord4 = "?!";
+// console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1]);
+
+
+// console.log("a string".slice(1, 5))
+
+// let longString = "My long string is long";
+// console.log(longString.slice(3, 14));
+// console.log(longString.slice(3));
+
+// console.log("Hello there, how are you doing?".toUpperCase());
+// console.log("Hello there, how are you doing?".toLowerCase());
+
+// let sentence = "hELlo THERE, hOW ARE yOu doINg?";
+// console.log(sentence[0].toUpperCase() + sentence.slice(1).toLowerCase());
+
+
+// let hadShower = false;
+// let hasBackPack = true;
+// let gotoSchool = hadShower && hasBackPack;
+// console.log(gotoSchool);
+
+// let hasApple = false;
+// let hasOrange = true;
+
+// let gotoSchool= hasApple || hasOrange;
+// console.log(gotoSchool);
+
+// let isWeekend = true;
+// let needToShowerToday = !isWeekend;
+// console.log(needToShowerToday);
+
+// let isWeekend = true;
+// let hadShower = true;
+// let hasApple = true;
+// let hasOrange = false;
+
+// let shouldGoToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
+// console.log(shouldGoToSchool);
+
+// let height = 60;
+// let heightRestriction = 60;
+// let mayRide = height >= heightRestriction;
+// console.log(mayRide);
+
+// let height = 60;
+// let heightRestriction = 48;
+// let mayRide = height <= heightRestriction;
+// console.log(mayRide);
+
+// let mySecretNumber = 5;
+// let chicoGuess = 3;
+// console.log(mySecretNumber == chicoGuess);
+// let harpoGuess = 7;
+// console.log(mySecretNumber == harpoGuess);
+// let grouchoGuess = 5;
+// console.log(mySecretNumber === grouchoGuess);
+
+// let stringNumber = "5";
+// let actualNumber = 5;
+// console.log(stringNumber === actualNumber);
+// console.log(stringNumber == actualNumber);
+
+
+//Try It Out
+
+let age = 12;
+let accompanied = true;
+let allowedIn = age >= 13 || accompanied;
+console.log(allowedIn);
+
+
+
+
+
+
+
+
+
+
+
+
+
