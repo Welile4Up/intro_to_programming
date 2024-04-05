a583e8ddb31363528fe32186a17bceae3f424a8c
Welile4Up <166120395+Welile4Up@users.noreply.github.com>
2024-04-05 16:25:37 +0200

    Add files via upload

---
+++ kidsbook/chapter7/chapter7.js
@@ -0,0 +1,13 @@
+//PROMPT
+// let name = prompt ("What is your name?");
+// //alert(`Hello ${name}`);
+// alert("Hello " + name);
+
+//CONFIRM
+let likesCats = confirm("Do you like cats?");
+console.log(likesCats);
+if (likesCats) {
+    alert("You're a cool cat!");
+} else {
+    alert("Yeah, that's fine. You're still cool!");
+}
