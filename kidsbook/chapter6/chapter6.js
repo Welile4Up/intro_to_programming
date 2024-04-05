3e14782a00140b7e2113ace237471c10603778f7
Welile4Up <166120395+Welile4Up@users.noreply.github.com>
2024-04-05 16:21:56 +0200

    Add files via upload

---
+++ kidsbook/chapter6/chapter6.js
@@ -0,0 +1,74 @@
+// let name = prompt("What is your name?");
+// // console.log("Hello " + name);
+// alert(`Hello ${name}`);
+// if (name.length > 7) {
+//     alert("Wow, you have a REALLY long name!");
+// }else{
+//     alert("Your name isn't very long");
+// }
+
+// let lemonChicken = false;
+// let beefWithBlackBean = false;
+// let sweetAndSourPork = false;
+
+// if (lemonChicken) {
+//     alert("Great! I'm having lemon chicken!");
+// } else if (beefWithBlackBean) {
+//     alert("I'm having the beef.");
+// } else if (sweetAndSourPork) {
+//     alert("OK, I'll have the pork.");
+// } else{
+//     alert("Well, I guess I'll have the rice then."); 
+// }
+
+//Try It Out
+//Do this later
+
+// let sheepCounted = 0;
+// while (sheepCounted < 10) {
+//     console.log(`I have counted ${sheepCounted} sheep!`);
+//     sheepCounted++;
+// }
+// console.log("Zzzzzzzzzzzz");
+
+// let count = 10;
+// while(count > 0) {
+//     alert(count);
+//     count--;
+// }
+// alert("Happy New Year!");
+
+
+// for (let count = 10; count > 0; count--) {
+//     alert(count);   
+// }
+// alert("Happy New Year!");
+
+
+// for (let sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+//     console.log(`I have counted ${sheepCounted} sheep!`);
+// }
+// console.log("Zzzzzzzzzzzz");
+
+// for(let i = 0, timesToSayHello = 5; i < timesToSayHello; i++){
+//     console.log("Hello");
+// }
+
+// let animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+
+// for(let i = 0; i < animals.length; i++){
+//     console.log(`This zoo contains a ${animals[i]}`);
+// }
+
+// let name = "Nick";
+// for(let i = 0; i < name.length; i++){
+//     console.log(`This name contains a ${name[i]}`);
+// }
+
+// for(let x = 2; x < 10000; x = x * 2){
+//     console.log(x);
+// }
+
+//Try It Out
+//Powers of three
+//Do later
