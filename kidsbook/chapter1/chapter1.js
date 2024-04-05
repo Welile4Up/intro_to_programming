4ddb426e7c6ae129b583ed796c0e4ad7d258a28a
Welile4Up <166120395+Welile4Up@users.noreply.github.com>
2024-04-05 14:36:30 +0200

    Add files via upload

---
+++ kidsbook/chapter1/chapter1.js
@@ -0,0 +1,9 @@
+/* console.log(3+4);
+        Draw as many cats as you want! */
+
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+drawCats(4); // You can put any number here instead of 10.
